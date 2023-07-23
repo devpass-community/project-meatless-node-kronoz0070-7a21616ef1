c966281700a59a1236aaf5b405f33614683520cb
JailsonJr-BR <jailsonjunior513@gmail.com>
2023-07-23 18:02:43 -0300

    feat: add product service

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,14 +1,47 @@
-const { insertQuery, selectQuery } = require('../config/configDB');
+const { BadRequestError } = require("../utils/helpers/error-helper");
+const {
+  createRepository,
+  findByIdRepository,
+  findByKeyRepository,
+} = require("../database/repositories");
 
-const getProducts = async() => {
-    // TODO: Implement method
+const { insertQuery, selectQuery } = require("../config/configDB");
+
+const getProducts = async (id) => {
+  const query = findByKeyRepository("Products", "restaurant_id", id);
+  
+  const queryExistsRestaurants = findByIdRepository("Restaurants", id);
+  const checkExistsRestaurants = await selectQuery(queryExistsRestaurants);
+
+  if (!checkExistsRestaurants.length) {
+    throw new BadRequestError("Restaurant not found");
+  }
+
+  const products = await selectQuery(query);
+
+  return products;
 };
 
-const addProduct = async(product) => {
-    // TODO: Implement method
+const addProduct = async (product) => {
+  const queryExistsRestaurants = findByIdRepository(
+    "Restaurants",
+    product.restaurant_id
+  );
+
+  const query = createRepository("Products", product);
+
+  const checkExistsRestaurants = await selectQuery(queryExistsRestaurants);
+
+  if (!checkExistsRestaurants.length) {
+    throw new BadRequestError("Restaurant not found");
+  }
+
+  await insertQuery(query);
+
+  return product;
 };
 
 module.exports = {
-    getProducts,
-    addProduct
+  getProducts,
+  addProduct,
 };
