aa6d7b31c9b5002d25710ff7bf64fd164319639e
JailsonJr-BR <jailsonjunior513@gmail.com>
2023-07-23 17:20:27 -0300

    feat: add middleware app error handler

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const cors = require('cors')
-const { productRouter } = require('./routes/productRouter')
-const { restaurantRouter } = require('./routes/restaurantRouter')
-const { shipmentRouter } = require('./routes/shipmentRouter')
-const { userRouter } = require('./routes/userRouter')
-const { orderRouter } = require('./routes/orderRouter')
-
-const port = 3000
+require("express-async-errors");
+const AppError = require("./utils/errors/AppError");
+const express = require("express");
+const cors = require("cors");
+const { productRouter } = require("./routes/productRouter");
+const { restaurantRouter } = require("./routes/restaurantRouter");
+const { shipmentRouter } = require("./routes/shipmentRouter");
+const { userRouter } = require("./routes/userRouter");
+const { orderRouter } = require("./routes/orderRouter");
 
 const app = express();
 app.use(express.json());
@@ -18,6 +18,24 @@ app.use(shipmentRouter);
 app.use(userRouter);
 app.use(orderRouter);
 
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
+
+const port = 3000;
+
 app.listen(port, () => {
-    console.log(`Server up and running on port: ${port}`)
-})
+  console.log(`Server up and running on port: ${port}`);
+});
