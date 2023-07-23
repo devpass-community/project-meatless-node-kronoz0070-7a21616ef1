f12eb3027bb0aecb6b2973ff86feda0de60c6947
JailsonJr-BR <jailsonjunior513@gmail.com>
2023-07-23 18:02:21 -0300

    feat: add product controller

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,14 +1,43 @@
-const { addProduct, getProducts } = require('../services/productService');
+const { addProduct, getProducts } = require("../services/productService");
+const { MissingParamError } = require("../utils/helpers/error-helper");
 
-const getProductsEndpoint = async(req, res) => {
-    // TODO: Implement method
+const getProductsEndpoint = async (req, res) => {
+  const { restaurantId } = req.params;
+  const products = await getProducts(restaurantId);
+
+  return res.json(products);
 };
 
-const addProductEndpoint = async(req, res) => {
-    // TODO: Implement method
+const addProductEndpoint = async (req, res) => {
+  const { restaurantId } = req.params;
+  const { description, full_description, category, price } = req.body;
+
+  const requiredFields = [
+    "description",
+    "full_description",
+    "category",
+    "price",
+  ];
+
+  for (const field of requiredFields) {
+    if (!req.body[field]) {
+      throw new MissingParamError(field);
+    }
+  }
+
+  const product = await addProduct({
+    description,
+    full_description,
+    category,
+    price,
+    restaurant_id: restaurantId,
+    active: true,
+  });
+
+  return res.status(201).json(product);
 };
 
-module.exports = { 
-    getProductsEndpoint, 
-    addProductEndpoint 
+module.exports = {
+  getProductsEndpoint,
+  addProductEndpoint,
 };
